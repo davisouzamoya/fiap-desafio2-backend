9d7da02395dcecde37e3dbcc4f760737d307f257
Marlon Vieira <marlonvsouza@gmail.comn>
2025-01-20 08:44:36 -0300

    Configuração de CI/CD com GitHub Actions e ajustes nos arquivos de teste e configuração

--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -1,15 +1,13 @@
-import express from 'express'
+import express from 'express';
 const router = express.Router();
-import postsRoute from '../controllers/postsController.js'
-import ensureAuthenticated from '../middleware/ensureAuthenticated.js'
+import postsRoute from '../controllers/postsController.js';
+import ensureAuthenticated from '../middleware/ensureAuthenticated.js';
 
-
-router.get("/",ensureAuthenticated, postsRoute.listPosts); 
-router.get("/search",ensureAuthenticated, postsRoute.searchPosts); 
-router.get("/:id",ensureAuthenticated, postsRoute.getPostById); 
-router.put("/change",ensureAuthenticated, postsRoute.updatePost); 
-router.post("/create",ensureAuthenticated, postsRoute.createPost); 
-router.delete("/delete/:id",ensureAuthenticated, postsRoute.deletePost); 
-
+router.get('/', ensureAuthenticated, postsRoute.listPosts);
+router.get('/search', ensureAuthenticated, postsRoute.searchPosts);
+router.get('/:id', ensureAuthenticated, postsRoute.getPostById);
+router.post('/', ensureAuthenticated, postsRoute.createPost); // Ajustado para seguir RESTful
+router.put('/:id', ensureAuthenticated, postsRoute.updatePost); // Ajustado para seguir RESTful
+router.delete('/:id', ensureAuthenticated, postsRoute.deletePost); // Ajustado para seguir RESTful
 
 export default router;
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,16 +1,14 @@
-import express from 'express'
+import express from 'express';
 const router = express.Router();
-import userRoute from '../controllers/usersController.js'
+import userRoute from '../controllers/usersController.js';
 import ensureAuthenticated from '../middleware/ensureAuthenticated.js';
 
-
-
-router.get("/",ensureAuthenticated, userRoute.listUsers); 
-router.get("/:id",ensureAuthenticated, userRoute.getUserById); 
-router.put("/change",ensureAuthenticated, userRoute.updateUser); 
-router.post("/create", userRoute.createUser); 
+// Rotas de usuários
+router.get("/", ensureAuthenticated, userRoute.listUsers); 
+router.get("/:id", ensureAuthenticated, userRoute.getUserById); 
+router.put("/change", ensureAuthenticated, userRoute.updateUser); 
+router.post("/register", userRoute.createUser); // Corrigido de "/create" para "/register"
 router.post("/login", userRoute.loginUser); 
-router.delete("/delete/:id",ensureAuthenticated, userRoute.deleteUser); 
-
+router.delete("/delete/:id", ensureAuthenticated, userRoute.deleteUser);
 
 export default router;
